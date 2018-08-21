cca0245daeec5cc98a97eb07763cd9431373bd5e
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2018-08-21 10:38:55 +0100

    Added file downloader

---
+++ BloomFilter/BloomFilter/FileDownloader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomFilter
+{
+    public class FileDownloader
+    {
+        public List<string> WordList { get; set; }
+
+
+        public void Download()
+        {
+            var wc = new WebClient();
+            var data = wc.OpenRead("http://codekata.com/data/wordlist.txt");
+
+            var sr = new StreamReader(data);
+
+            string currentLine;
+
+            do
+            {
+                currentLine = sr.ReadLine();
+
+                WordList.Add(currentLine);
+            }
+
+            while (currentLine != null);
+
+            data.Close();
+        }
+        
+    }
+}
